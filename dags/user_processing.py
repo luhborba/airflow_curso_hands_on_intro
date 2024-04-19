aa9492456f8859f5ad20de6129eaff91eb009518
LucianoBorba <luhborbafilho@gmail.com>
2024-04-19 14:25:44 -0300

    feat: criando create table postgres operator

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,6 +1,26 @@
-from airflow import DAG
-
 from datetime import datetime
 
-with DAG('user_processing', start_date=datetime(2024, 1, 1), schedule_interval='@daily', catchup=False) as dag:
-    pass
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+with DAG(
+    "user_processing",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    
+    create_table = PostgresOperator(
+        task_id="create_table",
+        postgres_conn_id="postgres",
+        sql="""
+            CREATE TABLE IF NOT EXISTS users (
+                firstname text NOT NULL,
+                lastname text NOT NULL,
+                country text NOT NULL,
+                username text NOT NULL,
+                password text NOT NULL,
+                email text NOT NULL
+            );
+        """,
+    )
