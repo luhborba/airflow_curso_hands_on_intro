a22efb2c301f6e2fc500384863acbbf043b54bff
LucianoBorba <luhborbafilho@gmail.com>
2024-04-20 20:24:08 -0300

    feat: utilizando taskgroups

--- dags/group_dag.py
+++ dags/group_dag.py
@@ -1,8 +1,8 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.subdag import SubDagOperator
-from subdagas.subdag_downloads import subdag_downloads
-from subdagas.subdag_transforms import subdag_transforms
+from groups.group_downloads import download_tasks
+from groups.group_transforms import transforms_tasks
+
 
  
 from datetime import datetime
@@ -10,12 +10,8 @@ from datetime import datetime
 with DAG('group_dag', start_date=datetime(2022, 1, 1), 
     schedule_interval='@daily', catchup=False) as dag:
 
-    args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
-
-    downloads = SubDagOperator(
-        task_id='downloads',
-        subdag=subdag_downloads(dag.dag_id, 'downloads', args)
-    )
+    
+    downloads = download_tasks()
  
  
     check_files = BashOperator(
@@ -23,10 +19,7 @@ with DAG('group_dag', start_date=datetime(2022, 1, 1),
         bash_command='sleep 10'
     )
  
-    transforms = SubDagOperator(
-        task_id='transforms',
-        subdag=subdag_transforms(dag.dag_id, 'transforms', args)
-    )
+    transforms = transforms_tasks()
     
  
     downloads >> check_files >> transforms
--- dags/groups/group_transforms.py
+++ dags/groups/group_transforms.py
@@ -1,11 +1,10 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.utils.task_group import TaskGroup
 
-def subdag_transforms(parent_dag_id, child_dag_id, args):
-    with DAG(f'{parent_dag_id}.{child_dag_id}',
-             schedule_interval=args['schedule_interval'],
-             start_date=args['start_date'],
-             catchup=args['catchup']) as dag:
+def transforms_tasks():
+        
+    with TaskGroup('transforms', tooltip="Transform tasks") as group:
         
         transform_a = BashOperator(
             task_id='transform_a',
@@ -22,4 +21,6 @@ def subdag_transforms(parent_dag_id, child_dag_id, args):
             bash_command='sleep 10'
         )
 
-        return dag
+        return group
+    
+        
